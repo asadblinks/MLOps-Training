f7aa164421b4218b2272d498b03d0c7b1e83a0b8
Asad Mehmood <asadmahmood16@hotmail.com>
2022-10-14 11:15:14 +0500

    updates to 09 folder

--- 08 ECR/list_s3_objects.py
+++ 08 ECR/list_s3_objects.py
@@ -1,9 +1,8 @@
-import boto3
+# import boto3
+# s3 = boto3.resource("s3")
+# bucket = s3.Bucket("mlops-aws")
+# for each in bucket.objects.all():
+#     print(each.key)
 import os
-s3 = boto3.resource("s3")
-bucket = s3.Bucket("mlops-aws-asad")
-for each in bucket.objects.all():
-    print(each.key)
-
 print(os.environ["AWS_ACCESS_KEY_ID"])
 print(os.environ["AWS_SECRET_ACCESS_KEY"])
